98cd6faeb14720bc70cd07149237cddcd82582bb
g-grandis <59686523+g-grandis@users.noreply.github.com>
2021-03-02 16:39:24 +0100

    added download feature

--- gg_homepage/src/Markdown.js
+++ gg_homepage/src/Markdown.js
@@ -3,6 +3,16 @@ import Form from 'react-bootstrap/Form'
 import ReactMarkdown from 'react-markdown'
 import Button from 'react-bootstrap/Button'
 
+
+    const  downloadTxtFile = () => {
+        const element = document.createElement("a");
+        const file = new Blob([document.getElementById('input').value],    
+        {type: 'text/plain;charset=utf-8'});
+        element.href = URL.createObjectURL(file);
+        element.download = "myFile.md";
+        document.body.appendChild(element);
+        element.click();
+      }
 const Markdown = () => {
     const [markdown, setMarkdown] = useState('');
     const [marckdownPreview, setMarkdownPreview] = useState('');
@@ -13,14 +23,20 @@ const Markdown = () => {
       const print = () => {
             setMarkdownPreview (markdown);
       }
+
     return (<div>
-        
+        <div>
+     </div>
         <Form>
             <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Markdown text area</Form.Label>
-                <Form.Control as="textarea" rows={3} onChange={handleChange}/>
+                <Form.Control id="input" as="textarea" rows={3} onChange={handleChange}/>
             </Form.Group>
             <Button onClick={print} >Preview</Button>
+            &nbsp;
+            <Button 
+            variant="secondary"
+            onClick={downloadTxtFile} >Download</Button>
         </Form>
         <p> <ReactMarkdown>{marckdownPreview}</ReactMarkdown> </p>
         
